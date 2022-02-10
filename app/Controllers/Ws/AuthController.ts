5078339fa9856d750a9685ba51d15fa6a705b7ab
motopeco <peco@pelife.jp>
2022-02-11 06:20:15 +0900

    ユーザモデル追加

--- app/Controllers/Ws/AuthController.ts
+++ app/Controllers/Ws/AuthController.ts
@@ -1,10 +1,12 @@
 import { Socket } from 'socket.io'
 import { auth } from 'firebase-admin'
 import DecodedIdToken = auth.DecodedIdToken
+import User from 'App/Models/User'
 
 export default class AuthController {
   public async login(socket: Socket, idToken: DecodedIdToken) {
     const uid = idToken.uid
+    await User.createUserIfNotExist(uid)
     socket.emit('auth', 'ok')
   }
 }
---
+++ app/Models/User.ts
@@ -0,0 +1,26 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { uid } from 'uid/secure'
+
+export default class User extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public uid: string
+
+  @column()
+  public name: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  public static async createUserIfNotExist(firebaseUID: string) {
+    const name = uid()
+
+    await User.firstOrCreate({ uid: firebaseUID }, { name })
+  }
+}
